642efdc26519dd0de52e1a483d6f0f3d75e35579
LucazzP <lucashpolazzo2013@gmail.com>
2020-01-10 20:34:02 -0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:desafio_campo_minado/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() => runApp(ModularApp(module: AppModule()));
